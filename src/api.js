5826a7d5673085fd6eeb596a7a4f3b9b5637d723
Dharshika Sivakumar <ptw.karl@gmail.com>
2023-01-06 12:43:37 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ff99d2464fmsh54e8084b3b6aaf2p1d4360jsnde81c47fb312',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY="f0321024ebed1a2afd3cacecf204b22e";
+
